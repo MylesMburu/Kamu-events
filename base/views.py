f22cd71c089ccc0b903e43429606248f3afde447
MylesMburu <ngicumyles@gmail.com>
2023-04-17 22:04:18 +0300

    .

--- base/views.py
+++ base/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Event
+from .forms import EventForm
 
 # events = [
 #     {'id': 1 , 'name':'Data science I4G'},
@@ -22,5 +23,10 @@ def event(request, pk):
     return render(request,'base/event.html', context)
 
 def createEvent(request):
-    context = {}
+    if request.method == 'POST':  #getting the POST request from the form
+        form = EventForm(request.POST)
+        if form.is_valid:
+            form.save()     #saves the data from the form in the database
+    form = EventForm()
+    context = {'form': form}
     return render (request,'base/event_form.html', context)
