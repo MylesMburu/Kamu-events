38cb0bd481c3cccd46e8620ec6df946304615dd2
MylesMburu <ngicumyles@gmail.com>
2023-05-04 20:51:54 +0300

    tailwind ish ish

---
+++ tailwind/tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "../base/**/*.{html,js}",
+    "../tailwind/*.{html,js}"
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
